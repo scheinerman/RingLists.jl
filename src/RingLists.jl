7f08b3dea3761a274b6afe1943fb2a724f3a2e1f
Ed Scheinerman <ers@jhu.edu>
2018-08-28 08:03:08 -0400

    Very beginning

--- src/RingLists.jl
+++ src/RingLists.jl
@@ -1,5 +1,23 @@
 module RingLists
+export RingList 
 
-greet() = print("Hello World!")
+struct RingList{T}
+    data::Dict{T,T}
+    function RingList{T}(elts::Vector{T}) where T
+        data = Dict{T,T}()
+        n = length(elts)
+        for i=1:n-1
+            a = elts[i]
+            b = elts[i+1]
+            data[a]=b
+        end
+        a = elts[n]
+        b = elts[1]
+        data[a] = b
+        new(data)
+    end
+end
+
+# greet() = print("Hello World!")
 
 end # module
