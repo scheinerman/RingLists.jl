bd9ae245e97c133c3387822e2a8b9652526d5394
Ed Scheinerman <ers@jhu.edu>
2018-08-28 08:35:22 -0400

    Basics

--- src/RingLists.jl
+++ src/RingLists.jl
@@ -1,21 +1,43 @@
 module RingLists
-export RingList 
+export RingList
 
 struct RingList{T}
     data::Dict{T,T}
-    function RingList{T}(elts::Vector{T}) where T
-        data = Dict{T,T}()
-        n = length(elts)
-        for i=1:n-1
-            a = elts[i]
-            b = elts[i+1]
-            data[a]=b
-        end
-        a = elts[n]
-        b = elts[1]
-        data[a] = b
-        new(data)
+end
+
+function RingList(T::Type=Any)
+    return RingList(Dict{T,T}())
+end
+
+function RingList(vals::Vector{T}) where T
+    d = Dict{T,T}()
+    n = length(vals)
+    if n == 0
+        return RingList(d)
     end
+    if n==1
+        a = vals[1]
+        d[a] = a
+        return RingList(d)
+    end
+    for i=1:n-1
+        a = vals[i]
+        b = vals[i+1]
+        d[a] = b
+    end
+    a = vals[end]
+    b = vals[1]
+    d[a] = b
+    return RingList(d)
+end
+
+function RingList(x...)
+    return RingList(collect(x))
+end
+
+function RingList{T}() where T 
+    d = Dict{T,T}()
+    return RingList(d)
 end
 
 # greet() = print("Hello World!")
