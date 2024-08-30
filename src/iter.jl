1ecd0ef810195cb822a97d44156eddab65cb75db
Ed Scheinerman <ers@jhu.edu>
2024-08-30 14:31:11 -0400

    Documenter

--- src/iter.jl
+++ src/iter.jl
@@ -1,19 +1,17 @@
 import Base.iterate
 
 function iterate(a::RingList)
-    if length(a)==0
-        return nothing 
-    end 
+    if length(a) == 0
+        return nothing
+    end
     f = first(a)
-    return f,f
-end 
+    return f, f
+end
 
 function iterate(a::RingList, st)
     y = a[st]
-    if y==first(a)
-        return nothing 
-    end 
-    return y,y 
-end 
-
-
+    if y == first(a)
+        return nothing
+    end
+    return y, y
+end
--- src/show.jl
+++ src/show.jl
@@ -4,34 +4,17 @@ import Base: show, string
 function string(a::RingList)
     if length(a) == 0
         return "[ ]"
-    end 
+    end
     result = "[ "
     arrow = " → "
-    for x in a 
+    for x in a
         result = result * string(x) * arrow
     end
-    result = result * string(first(a,true)) * " ]"
-    return result 
-end 
-
-    
-function show(io::IO, a::RingList)
-    print(io,string(a))
+    result = result * string(first(a, true)) * " ]"
+    return result
 end
 
 
-
-
-# function show(io::IO, a::RingList{T}) where {T}
-#     v = Vector(a)
-#     result = "RingList{$T}("
-#     n = length(a)
-#     for i = 1:n
-#         result *= "$(v[i])"
-#         if i < n
-#             result *= ","
-#         end
-#     end
-#     result *= ")"
-#     print(io, result)
-# end
+function show(io::IO, a::RingList)
+    print(io, string(a))
+end
